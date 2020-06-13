c9a94af7e302e4a699ddff11f6470e040fd4bea2
opalmay <65673442+opalmay@users.noreply.github.com>
2020-06-13 20:19:46 +0300

    final version

--- js/gallery.js
+++ js/gallery.js
@@ -1,15 +1,39 @@
 'use strict'
-const imgCount = 25;
+var filterStr = '';
 
 function initGallery() {
     let gallery = document.querySelector('.gallery');
     gallery.classList.remove('hidden')
     document.querySelector('.editor').classList.add('hidden');
+    generateKeywords();
+    generateGallery();
+}
+function generateGallery() {
+    let gallery = document.querySelector('.gallery');
     let strHtml = '';
-    for (let i = 1; i <= imgCount; i++) {
-        strHtml += `<img onclick="onImgClick(${i})" src="img/${i}.jpg">`;
+    var filteredKeywords = getFilteredImages(filterStr);
+
+    console.log(filterStr);
+    for (let i = 0; i < filteredKeywords.length; i++) {
+        strHtml += `<img onclick="onImgClick(${filteredKeywords[i].id})" src="${getMemeImgUrl(filteredKeywords[i].id)}">`;
     }
+    console.log(filteredKeywords);
     gallery.innerHTML = strHtml;
+}
+function generateKeywords() {
+    var mapTags = getKeywords();
+    var strHtml = '';
+    for (const key in mapTags) {
+        var fontSize = 20 + mapTags[key] / 2;
+        strHtml += `<button style="font-size:${fontSize}px" onClick="onFilterClick('${key}')">${key}</button>`;
+    }
+    document.querySelector('.tags').innerHTML = strHtml;
+}
+function onFilterClick(filter) {
+    filterStr = filter;
+    keywordClicked(filter);
+    generateKeywords();
+    generateGallery();
 }
 function onImgClick(id) {
     initService(id);
--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ var gElImg;
 var gMoveDifferenceX, gMoveDifferenceY;
 
 function init() {
+    initService();
     initGallery();
 }
 
@@ -26,6 +27,7 @@ function initEditor() {
     gElCanvas = document.querySelector('canvas');
     gCtx = gElCanvas.getContext('2d');
     resizeCanvas();
+    onUpdateSelectedLine();
     drawAll();
 }
 function drawAll() {
@@ -77,10 +79,7 @@ function onCanvasClick(ev) {
         };
         gLines.push(gSelectedLine);
     }
-    document.querySelector('.fontSelect').value = gSelectedLine.font;
-    var lineText = document.querySelector('.lineText');
-    lineText.value = gSelectedLine.text;
-    lineText.focus();
+    onUpdateSelectedLine();
     drawAll();
 }
 function onInput(ev) {
@@ -95,9 +94,22 @@ function onRemoveLine() {
     removeLine(gSelectedLine);
     gLines = getLines();
     gSelectedLine = gLines[0];
+    onUpdateSelectedLine();
     drawAll();
 }
-function onDownload() {
+function onUpdateSelectedLine() {
+    let font = '';
+    let text = '';
+    var elLineText = document.querySelector('.lineText');
+    if (gSelectedLine) {
+        font = gSelectedLine.font;
+        text = gSelectedLine.text;
+        elLineText.focus();
+    }
+    document.querySelector('.fontSelect').value = font;
+    elLineText.value = text;
+}
+function onDownload(elLink) {
     var tempSelectedLine = gSelectedLine;
     gSelectedLine = null;
     drawAll();
@@ -108,8 +120,44 @@ function onDownload() {
         link.click();
         gSelectedLine = tempSelectedLine;
     }, 50);
-
 }
+// function onShareToFB() {
+//     var tempSelectedLine = gSelectedLine;
+//     gSelectedLine = null;
+//     drawAll();
+//     setTimeout(() => {
+//         // var link = document.createElement('a');
+//         // link.download = 'meme.jpg';
+//         console.log(document.querySelector('canvas').toDataURL())
+//         let u = uploadImage(document.querySelector('canvas').toDataURL());
+
+//         // link.click();
+//         gSelectedLine = tempSelectedLine;
+//         // let t = 'Check out the meme I\'ve made!';
+//         // window.open('http://www.facebook.com/sharer.php?u=' + encodeURIComponent(u) + '&t=' + encodeURIComponent(t), 'sharer', 'toolbar=0,status=0,width=626,height=436');
+//     }, 50);
+// }
+// function uploadImage(file){
+//     var fd = new FormData();
+//     fd.append("image", file); // Append the file
+//     // fd.append("Authorization", "Client-ID {{28587fd13d53d7e}}");
+//     // Get your own key: http://api.imgur.com/
+  
+//     // Create the XHR (Cross-Domain XHR FTW!!!)
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("POST", "https://api.imgur.com/3/image"); // Boooom!
+//     xhr.setRequestHeader('Authorization','Client-ID 28587fd13d53d7e');
+//     xhr.onload = function() {
+//       // Big win!
+//       // The URL of the image is:
+//       console.log(xhr.responseText);
+//       return JSON.parse(xhr.responseText).upload.links.imgur_page;
+//      }
+//      // Ok, I don't handle the errors. An exercice for the reader.
+//      // And now, we send the formdata
+//      xhr.send(file);
+//    }
+
 function onChangeSize(size) {
     if (!gSelectedLine) return;
     if (gSelectedLine.size + size < 12) return;
@@ -118,7 +166,7 @@ function onChangeSize(size) {
 }
 
 function onStartDragging(ev) {
-    const { x, y } = getRelativeCords(ev);
+    const { x, y } = getRelativeCoords(ev);
     let selectedLine = getLineByIndexes(x, y);
     if (!selectedLine) return
     gSelectedLine = selectedLine;
@@ -131,12 +179,12 @@ function onStartDragging(ev) {
 }
 function onMoveStuff(ev) {
     ev.preventDefault();
-    const { x, y } = getRelativeCords(ev);
+    const { x, y } = getRelativeCoords(ev);
     gSelectedLine.x = x - gMoveDifferenceX;
     gSelectedLine.y = y - gMoveDifferenceY;
     drawAll();
 }
-function getRelativeCords(ev) {
+function getRelativeCoords(ev) {
     const elRect = ev.target.getBoundingClientRect();
     return ev.type.includes('mouse') ? { x: ev.offsetX, y: ev.offsetY } :
         { x: ev.touches[0].pageX - elRect.left, y: ev.touches[0].pageY - elRect.top };
@@ -161,7 +209,6 @@ function getLineByIndexes(x, y) {
 function resizeCanvas() {
     var elContainer = document.querySelector('.canvas_container');
     gElCanvas.width = Math.min(gElImg.width, elContainer.offsetWidth);
-    // gElCanvas.width = elContainer.offsetWidth;
     gElCanvas.height = gElCanvas.width / (gElImg.width / gElImg.height);
 }
 function onChangeFont(el) {
